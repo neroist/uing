74e7a0bf9df3ebe8d00459d258bfc5d76714e246
Grave <pmpiga1@gmail.com>
2023-02-04 20:43:52 -0500

    fix table example

--- examples/table.nim
+++ examples/table.nim
@@ -1,4 +1,5 @@
 import std/random
+import std/sugar
 
 import uing
 from uing/rawui import nil
@@ -17,50 +18,40 @@ const
   COLUMN_PASSED = 5
   COLUMN_ACTION = 6
 
-var
-  progress: array[NUM_ROWS, array[NUM_COLUMNS, int]]
+let progress = collect: 
+  for row in 0..<NUM_ROWS:
+    rand(100)
 
 proc modelNumColumns(mh: ptr TableModelHandler, m: ptr rawui.TableModel): cint {.cdecl.} = NUM_COLUMNS
 proc modelNumRows(mh: ptr TableModelHandler, m: ptr rawui.TableModel): cint {.cdecl.} = NUM_ROWS
 
 proc modelColumnType(mh: ptr TableModelHandler, m: ptr rawui.TableModel, col: cint): TableValueType {.cdecl.} =
-  #echo "type"
-
-  if col in [COLUMN_ID, COLUMN_PROCESS, COLUMN_PASSED]:
+  if col in [COLUMN_PROCESS, COLUMN_PASSED]:
     result = TableValueTypeInt
   else:
     result = TableValueTypeString
 
 proc modelCellValue(mh: ptr TableModelHandler, m: ptr rawui.TableModel, row, col: cint): ptr rawui.TableValue {.cdecl.} =
-  if col == COLUMN_ID:
-    result = newTableValue($(row+1)).impl
-  elif col == COLUMN_PROCESS:
-    if progress[row][col] == 0:
-      progress[row][col] = rand(100)
-    result = newTableValue(progress[row][col]).impl
-  #elif col == COLUMN_PASSED:
-  #  if progress[row][col] > 60:
-  #    result = newTableValue(1).impl
-  #  else:
-  #    result = newTableValue(0).impl
-  elif col == COLUMN_ACTION:
-    result = newTableValue("Apply").impl
-  else:
-    result = newTableValue("row " & $row & " x col " & $col).impl
-
+  case col:
+    of COLUMN_ID:
+      result = newTableValue($(row+1)).impl
+    of COLUMN_PROCESS:
+      result = newTableValue(progress[row]).impl
+    of COLUMN_PASSED:
+      result = newTableValue(int(progress[row] > 60)).impl
+    of COLUMN_ACTION:
+      result = newTableValue("Apply").impl
+    else:
+      result = newTableValue("row " & $row & "x col " & $col).impl
 
 proc modelSetCellValue(mh: ptr TableModelHandler, m: ptr rawui.TableModel, row, col: cint, val: ptr rawui.TableValue) {.cdecl.} =
-  #echo "setCellValue"
-
-  if col == COLUMN_PASSED:
-    echo rawui.tableValueInt(val)
-  elif col == COLUMN_ACTION:
+  if col == COLUMN_ACTION:
     rawui.tableModelRowChanged(m, row)
 
+# has to be outside of main() or else we get a SIGSEGV???
 var
   mh: TableModelHandler
   p: TableParams
-  tp {.used.}: TableTextColumnOptionalParams 
 
 proc main() =
   var mainwin: Window
@@ -81,17 +72,17 @@ proc main() =
   mh.numColumns = modelNumColumns
   mh.columnType = modelColumnType
   mh.numRows = modelNumRows
-  mh.cellValue  = modelCellValue
+  mh.cellValue = modelCellValue
   mh.setCellValue = modelSetCellValue
 
-  p.model = rawui.newTableModel(addr mh)
+  p.model = newTableModel(addr mh).impl
   p.rowBackgroundColorModelColumn = 4
  
   let table = newTable(addr p)
-  table.addTextColumn("ID", COLUMN_ID, TableModelColumnNeverEditable, nil)
-  table.addTextColumn("First Name", COLUMN_FIRST_NAME, TableModelColumnAlwaysEditable, nil)
-  table.addTextColumn("Last Name", COLUMN_LAST_NAME, TableModelColumnAlwaysEditable, nil)
-  table.addTextColumn("Address", COLUMN_ADRESS, TableModelColumnAlwaysEditable, nil)
+  table.addTextColumn("ID", COLUMN_ID, TableModelColumnNeverEditable)
+  table.addTextColumn("First Name", COLUMN_FIRST_NAME, TableModelColumnAlwaysEditable)
+  table.addTextColumn("Last Name", COLUMN_LAST_NAME, TableModelColumnAlwaysEditable)
+  table.addTextColumn("Address", COLUMN_ADRESS, TableModelColumnAlwaysEditable)
   table.addProgressBarColumn("Progress", COLUMN_PROCESS)
   table.addCheckboxColumn("Passed", COLUMN_PASSED, TableModelColumnAlwaysEditable)
   table.addButtonColumn("Action", COLUMN_ACTION, TableModelColumnAlwaysEditable)
