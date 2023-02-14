9656ea0a52a07d75ab284d092dcda74fe3b35c83
t-h-rk-turner <71654487+t-h-rk-turner@users.noreply.github.com>
2023-02-14 05:37:09 -0500

    spacing

--- uing/genui.nim
+++ uing/genui.nim
@@ -12,63 +12,69 @@ proc `[]`(s: NimNode, x: Slice[int]): seq[NimNode] =
   newSeq(result, L)
   for i in 0..<L: result[i] = s[i + a]
 
-proc high(s: NimNode):int =
+proc high(s: NimNode): int =
   s.len-1
 
-template add*[SomeWidget: Widget](g:Group, child:SomeWidget) =
+template add*[SomeWidget: Widget](g: Group, child: SomeWidget) =
   g.`child=`child
 
-template add*[SomeWidget: Widget](g:Window, child:SomeWidget) =
+template add*[SomeWidget: Widget](g: Window, child: SomeWidget) =
   g.`child=`child
 
 macro genui*(args: varargs[untyped]): untyped =
   type WidgetArguments = object
-    identifier:NimNode
+    identifier: NimNode
     name: string
     addArguments: seq[NimNode]
     arguments: seq[NimNode]
     children: seq[WidgetArguments]
-    isStr:bool
-    isIdentified:bool
+    isStr: bool
+    isIdentified: bool
 
-  proc parseNode(node:NimNode):WidgetArguments
-  proc parseBracketExpr(bracketExpr:NimNode):WidgetArguments
-  proc parseChildren(stmtList:NimNode): seq[WidgetArguments] =
+  proc parseNode(node: NimNode): WidgetArguments
+  proc parseBracketExpr(bracketExpr: NimNode): WidgetArguments
+  proc parseChildren(stmtList: NimNode): seq[WidgetArguments] =
     result = @[]
+
     for child in stmtList:
       result.add parseNode(child)
 
-  proc parseCall(call:NimNode):WidgetArguments =
+  proc parseCall(call: NimNode): WidgetArguments =
     let hasAddArguments = call[0].kind == nnkBracketExpr
     let hasChildren = call[call.high].kind == nnkStmtList
+
     if hasAddArguments:
       result = parseBracketExpr(call[0])
     else:
       result.name = $call[0]
+
     if result.arguments == @[]:
       result.arguments = if hasChildren: call[1..<call.high] else: call[1..call.high]
     #else:
     #  for arg in if hasChildren: call[1..<call.high] else: call[1..call.high]:
     #    result.arguments.add arg
+
     result.children = if hasChildren: parseChildren(call[call.high]) else: @[]
 
-  proc parseBracketExpr(bracketExpr:NimNode):WidgetArguments =
+  proc parseBracketExpr(bracketExpr: NimNode): WidgetArguments =
     let hasArguments = bracketExpr[0].kind == nnkCall
     let hasChildren = bracketExpr[bracketExpr.high].kind == nnkStmtList
+
     if hasArguments:
       result = parseCall(bracketExpr[0])
     else:
       result.name = $bracketExpr[0]
+
     result.addArguments = if hasChildren: bracketExpr[1..<bracketExpr.high] else: bracketExpr[1..bracketExpr.high]
     result.children = if hasChildren: parseChildren(bracketExpr[bracketExpr.high]) else: @[]
 
-  proc parseInfix(infix:NimNode):WidgetArguments=
+  proc parseInfix(infix: NimNode): WidgetArguments =
     result = parseNode(infix[2])
     result.identifier = infix[1]
     if infix[infix.high].kind == nnkStmtList:
       result.children = parseChildren(infix[infix.high])
 
-  proc parseIdent(ident:NimNode):WidgetArguments=
+  proc parseIdent(ident: NimNode): WidgetArguments=
     result = WidgetArguments(
       identifier: nil,
       name: $ident,
@@ -77,18 +83,19 @@ macro genui*(args: varargs[untyped]): untyped =
       children: @[],
     )
 
-  proc parsePrefix(prefix:NimNode):WidgetArguments=
+  proc parsePrefix(prefix: NimNode): WidgetArguments=
     #assert prefix[0] == !"%", "Use % to identify"
+
     result = parseNode(prefix[1])
     result.isIdentified = true
 
-  proc parseString(str:NimNode):WidgetArguments=
+  proc parseString(str: NimNode): WidgetArguments=
     result = WidgetArguments(
       name: str.strVal,
       isStr: true
     )
 
-  proc parseNode(node:NimNode):WidgetArguments =
+  proc parseNode(node: NimNode): WidgetArguments =
     case node.kind:
     of nnkCall:
       result = parseCall(node)
@@ -111,9 +118,11 @@ macro genui*(args: varargs[untyped]): untyped =
     else:
       ident = value
 
-  proc createWidget(widget: var WidgetArguments):NimNode =
+  proc createWidget(widget: var WidgetArguments): NimNode =
     result = newStmtList()
-    var call:NimNode
+
+    var call: NimNode
+
     if widget.isIdentified:
       call = newIdentNode(widget.name)
     else:
@@ -121,7 +130,8 @@ macro genui*(args: varargs[untyped]): untyped =
       if widget.arguments != @[]:
         for arg in widget.arguments:
           call.add arg
-    if widget.identifier==nil:
+
+    if widget.identifier == nil:
       widget.identifier = genSym(nskLet)
       result.add nnkLetSection.newTree(
         nnkIdentDefs.newTree(
@@ -139,21 +149,27 @@ macro genui*(args: varargs[untyped]): untyped =
       else:
         var c = child
         let childCode = createWidget(c)
+
         for node in childCode:
           result.add node
+
         var addCall = newCall("add",widget.identifier, nnkExprEqExpr.newTree(
             ident"child",
             c.identifier
           )
         )
+
         for addArg in c.addArguments:
           addCall.add addArg
+
         result.add addCall
 
   let parsed = parseChildren(args[0])
   result = newStmtList()
+
   for widget in parsed:
     var w = widget
     let widgetCode = createWidget(w)
+
     for node in widgetCode:
       result.add(node)
